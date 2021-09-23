a57e8826e8a2f85cd6c070dcc39dcdaa681268b7
Nikolay Yakimov <root@livid.pp.ru>
2021-09-23 16:37:41 +0300

    Ignore known errors, tweaks

--- index.ts
+++ index.ts
@@ -7,57 +7,95 @@ import fetch from 'node-fetch'
 import Queue from 'promise-queue'
 
 const parse = (parser.default as any).default as typeof parser.default
-
-let cache = new Map<string, Promise<string>>()
 const domCache = new Map<string, parser.HTMLElement>()
-
 const readFile = promisify(fs.readFile)
-
 const queue = process.argv.slice(2)
-
 const fetchQueue = new Queue(5, Infinity)
 
-let hasErrors = false
+let writeKnownErrors = false
+if (queue[0] === "--write-known-errors") {
+  writeKnownErrors = true
+  queue.shift()
+}
+
+type Cache = Map<string, Promise<string>>
+type Errors = Set<ErrorRec>
+interface ErrorRec {
+  partialPath: string,
+  reference: string,
+  referenceText: string,
+  errorText: string,
+}
 
 loadCache()
+  .then(loadErrors)
   .then(processQueue)
   .then(saveCache)
+  .then(saveErrors)
   .then(exitWithCode)
 
-function exitWithCode() {
-  if (hasErrors) process.exit(1)
+function exitWithCode(errors: boolean) {
+  if (errors) process.exit(1)
   else process.exit(0)
 }
 
-async function loadCache() {
+async function loadCache(): Promise<Cache> {
   try {
     const inn = JSON.parse(fs.readFileSync('.claa-cache', {encoding: 'utf8'}))
-    cache = new Map(Object.entries(inn).map(([k,v]) => [k, Promise.resolve(v as string)]))
+    return new Map(Object.entries(inn).map(([k,v]) => [k, Promise.resolve(v as string)]))
   } catch (e) {
     console.log('Failed to load cache')
+    return new Map()
   }
 }
 
-async function saveCache() {
+async function loadErrors(cache: Cache): Promise<[Cache,Errors]> {
+  try {
+    const inn = JSON.parse(fs.readFileSync('.claa-known-errors', {encoding: 'utf8'}))
+    return [cache, new Set(inn)]
+  } catch (e) {
+    console.log('Failed to load known errors')
+    return [cache, new Set()]
+  }
+}
+
+async function saveCache([hasErrors, errors, cache]: [boolean, Errors, Cache]): Promise<[boolean, Errors]> {
   const out = Object.fromEntries(await
     Promise.all(Array.from(cache.entries()).map(async ([k, v]) => [k, await v])))
   fs.writeFileSync('.claa-cache', JSON.stringify(out))
+  return [hasErrors, errors]
 }
 
-async function processQueue(): Promise<void> {
-  if(queue.length === 0) return
+async function saveErrors([hasErrors, errors]: [boolean, Errors]): Promise<boolean> {
+  if(writeKnownErrors && hasErrors) {
+    const out = Array.from(errors)
+    fs.writeFileSync('.claa-known-errors', JSON.stringify(out,undefined,2))
+  }
+  return hasErrors
+}
+
+async function processQueue([cache, errors]: [Cache, Errors]): Promise<[boolean, Errors, Cache]> {
+  if(queue.length === 0) return [false, errors, cache]
+  let hasErrors = false
+  const checks = new Set(Array.from(errors.values()).map(v => JSON.stringify(v)))
   while (queue.length > 0) {
     const url = queue.shift()
-    const res = await checkLinks(url!)
+    const res = await checkLinks(cache, url!)
     for (const [p,h,t,r] of res) {
       if (r === true) continue
-      console.log(`${path.basename(p)} referencing ${t} (${h}) broken with ${r}\n`)
+      const partialPath = path.join(path.basename(path.dirname(p)), path.basename(p))
+      const errorText = 'code' in r ? (r as any)['code'] : r.toString()
+      const error = {errorText, partialPath, reference: h, referenceText: t}
+      if (checks.has(JSON.stringify(error))) continue
+      console.log(`${partialPath} referencing ${t} (${h}) broken with ${r}\n`)
+      if (writeKnownErrors) errors.add(error)
       hasErrors = true
     }
   }
+  return [hasErrors, errors, cache]
 }
 
-async function checkLinks(filepath: string): Promise<[string, string, string, true | object][]> {
+async function checkLinks(cache: Cache, filepath: string): Promise<[string, string, string, true | object][]> {
   const fileContent = await readFile(filepath, {encoding: 'utf8'})
   const dom = jsdomWithCache(filepath, fileContent)
   const dir = path.dirname(filepath)
@@ -77,13 +115,15 @@ async function checkLinks(filepath: string): Promise<[string, string, string, tr
         // do nothing
       } else if (href.startsWith('about:')) {
         // do nothing
+      } else if (el.parentNode.innerText.startsWith('Defined in ')) {
+        // do nothing
       } else if (el.innerText === 'Source') {
         // do nothing
       } else if (href.startsWith('#')) {
-        await checkHrefAndAnchor(dir, filepath, href.slice(1))
+        await checkHrefAndAnchor(cache, dir, filepath, href.slice(1))
       } else {
         const [url, hash] = href.split('#')
-        await checkHrefAndAnchor(dir, url, hash)
+        await checkHrefAndAnchor(cache, dir, url, hash)
       }
       return [filepath, href, el.innerText, true] as [string,string,string,true]
     } catch (e) {
@@ -92,18 +132,20 @@ async function checkLinks(filepath: string): Promise<[string, string, string, tr
   }))
 }
 
-async function checkHrefAndAnchor(dir: string, url: string, anchor: string) {
+async function checkHrefAndAnchor(cache: Cache, dir: string, url: string, anchor: string) {
   const urlRaw = url.match(/^https?:\/\//) !== null
     ? url
     : getPath(dir, url)
   const contents = url.match(/^https?:\/\//) !== null
-    ? await fetchWithCache(url)
+    ? await fetchWithCache(cache, url)
     : await readFile(getPath(dir, url), {encoding: 'utf8'})
-  if (anchor) {
+  if (anchor && url.match(/^https?:\/\/gitlab.com\//) === null) {
     const dom = jsdomWithCache(url, contents)
     const elem = dom.querySelector(`#${escape(anchor)}`)
     if (elem === null) {
-      throw new Error(`Anchor ${anchor} not found in ${url}`)
+      const err: any = new Error(`Anchor ${anchor} not found in ${url}`)
+      err.code = 'NOANCHOR'
+      throw err
     }
   }
   // console.log(url, anchor, 'OK')
@@ -121,7 +163,7 @@ function getPath(dir: string, url: string) {
   else return path.join(dir, url)
 }
 
-async function fetchWithCache(url: string) {
+async function fetchWithCache(cache: Cache, url: string) {
   let res = cache.get(url)
   if (res!==undefined) return res
   res = fetchQueue.add(async () => (await fetch(url)).text())
